59bd7d0ce0e7c79369835a57e66201fe6f80b62c
Joel Grant <joelmgrant@me.com>
2021-11-30 20:50:45 -0700

    Create routes for company and dojo show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/companies', to: 'companies#index'
+  get '/companies/:id', to: 'companies#show'
+
   get '/dojos', to: 'dojos#index'
+  get '/dojos/:id', to: 'dojos#show'
 end
