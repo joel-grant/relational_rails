d7a93cf13fd3ae84db27bdb6f8e805b845036082
Joel Grant <joelmgrant@me.com>
2021-12-02 13:11:12 -0700

    Create company/seed count

--- spec/features/companies/show_spec.rb
+++ spec/features/companies/show_spec.rb
@@ -4,16 +4,25 @@ RSpec.describe 'company show page' do
   describe 'when I visit /companies/:id' do
     it 'displays the company with id and its attributes' do
       company = Company.create!(name: "Johnny's Seeds", accepting_orders: true, years_active: 10)
-      company_2 = Company.create!(name: "Turing Seeds", accepting_orders: false, years_active: 6)
+      company_2 = Company.create!(name: "Turing Seeds", accepting_orders: false, years_active: 12)
+
       visit "/companies/#{company.id}"
-      
+
       expect(page).to have_content(company.name)
       expect(page).to have_content(company.accepting_orders)
       expect(page).to have_content(company.years_active)
 
       expect(page).to_not have_content(company_2.name)
-      expect(page).to_not have_content(company_2.accepting_orders)
-      expect(page).to_not have_content(company_2.years_active)
+    end
+
+    it 'displays the quantity of instructors in the Dojo' do
+      company = Company.create!(name: "Johnny's Seeds", accepting_orders: true, years_active: 10)
+      seed = company.seeds.create!(name: "Jessica", available: true, quantity: 3)
+      seed_2 = company.seeds.create!(name: "Jimmy", available: false, quantity: 55)
+
+      visit "/companies/#{company.id}"
+
+      expect(page).to have_content(company.seeds.count)
     end
   end
 end
