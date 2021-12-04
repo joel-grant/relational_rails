63c7cc7275224e12442788ba90283b914f4dab2f
Joel Grant <joelmgrant@me.com>
2021-12-03 19:10:42 -0700

    Create update dojos functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
   get '/companies', to: 'companies#index'
   get '/companies/new', to: 'companies#new'
   get '/companies/:id', to: 'companies#show'
-  post '/companies/', to: 'companies#create'
+  post '/companies', to: 'companies#create'
+  get '/companies/:id/edit', to: 'companies#edit'
+  patch '/companies/:id', to: 'companies#update'
 
   get '/dojos', to: 'dojos#index'
   get '/dojos/new', to: 'dojos#new'
   get '/dojos/:id', to: 'dojos#show'
   post '/dojos', to: 'dojos#create'
+  get '/dojos/:id/edit', to: 'dojos#edit'
+  patch '/dojos/:id', to: 'dojos#update'
 
   get '/instructors', to: 'instructors#index'
   get '/instructors/:id', to: 'instructors#show'
