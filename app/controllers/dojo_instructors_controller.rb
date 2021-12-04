dfe0f12b808af559372fb9c25d90db07913900a1
Karan Mehta <karanm645@gmail.com>
2021-12-04 15:34:04 -0600

    Implement Dojo/Instructor Creation

--- app/controllers/dojo_instructors_controller.rb
+++ app/controllers/dojo_instructors_controller.rb
@@ -1,6 +1,22 @@
 class DojoInstructorsController < ApplicationController
   def index
-    dojo = Dojo.find(params[:id])
-    @instructors = dojo.instructors
+    @dojo = Dojo.find(params[:id])
+    @instructors = @dojo.instructors
+  end
+
+  def new
+    @dojo = Dojo.find(params[:id])
+  end
+
+  def create
+    @dojo = Dojo.find(params[:id])
+    @instructor = @dojo.instructors.create!(instructor_params)
+
+    redirect_to "/dojos/#{@dojo.id}/instructors"
+  end
+
+  private 
+  def instructor_params
+    params.permit(:name, :payroll, :experience)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,25 @@ Rails.application.routes.draw do
   get '/companies', to: 'companies#index'
   get '/companies/new', to: 'companies#new'
   get '/companies/:id', to: 'companies#show'
-  post '/companies/', to: 'companies#create'
+  post '/companies', to: 'companies#create'
+  get '/companies/:id/seeds/new', to: 'company_seeds#new'
+  post '/companies/:id/seeds', to: 'company_seeds#create'
 
   get '/dojos', to: 'dojos#index'
   get '/dojos/new', to: 'dojos#new'
   get '/dojos/:id', to: 'dojos#show'
   post '/dojos', to: 'dojos#create'
+  get '/dojos/:id/instructors/new', to: 'dojo_instructors#new'
+  post '/dojos/:id/instructors', to: 'dojo_instructors#create'
 
   get '/instructors', to: 'instructors#index'
   get '/instructors/:id', to: 'instructors#show'
-
+  
   get '/seeds', to: 'seeds#index'
   get '/seeds/:id', to: 'seeds#show'
-
+  
   get '/dojos/:id/instructors', to: 'dojo_instructors#index'
-
+  
   get '/companies/:id/seeds', to: 'company_seeds#index'
 
 end
