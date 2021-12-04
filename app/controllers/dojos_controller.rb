8733acfd99a45e81b113a2c38ec128b7c88bb3c1
Joel Grant <joelmgrant@me.com>
2021-12-03 17:38:57 -0700

    Add Create Dojo functionality

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -6,4 +6,17 @@ class DojosController < ApplicationController
   def show
     @dojo = Dojo.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    dojo = Dojo.create!(dojo_params)
+    redirect_to '/dojos'
+  end
+
+  private
+  def dojo_params
+    params.permit(:name, :open, :rating)
+  end
 end
